488067dac9186b3ed3bb74f7d9c0885c3008f07c
milosnowcat <ramiro.alvarez@rahcode.com>
2023-09-21 18:54:04 -0600

    comments

--- main.py
+++ main.py
@@ -52,11 +52,16 @@ from PIL import Image, ImageTk
 import random
 
 def update_image():
+    """
+    The function `update_image` updates the image displayed on the `image_panel` with a randomly
+    selected dice image.
+    """
     number = random.randint(1, 6)
     dice_image = ImageTk.PhotoImage(Image.open(f"assets/images/diceMagenta{number}.png").resize((240, 240)))
     image_panel.config(image=dice_image)
     image_panel.image = dice_image
 
+# The code is creating a graphical user interface (GUI) using the Tkinter library in Python.
 root = ttk.Window(themename="vapor")
 root.title("Dice.py")
 root.geometry("700x400")
